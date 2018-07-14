0eaa024a2163a0517663bbdcb19e1ae5699e6ccf
imani alexis <alexis.bailey37@gmail.com>
2018-07-14 19:28:49 -0400

    added api calls for my three pokemon

--- main.js
+++ main.js
@@ -9,18 +9,63 @@
 
 
 class Pokemon {
-    constructor(name)
-    let name = this.name
+    constructor(name,abilities,moves,types) {
+        this.name = name
+        this.abilities = abilities
+        this.moves = moves
+        this.types = types
+    }
 }
 
 
 
 
 
+let golbat;
 
-axios.get('https://pokeapi.co/api/v2/pokemon/42/').then((response) => {
+// Golbat API Call
+axios.get("https://pokeapi.co/api/v2/pokemon/42/").then((response) => {
     let data = response.data
-    let name = data.name
-    console.log(name)
+    let pokemonName = data.name
+    let abilities = data.abilities.map( (element) => element.ability.name)
+    let moves = data.moves.map((element) =>  element.move.name)
+    let types = data.types.map((element) => element.type.name)
+  
+     golbat = new Pokemon(pokemonName, abilities,moves,types)
+})
 
+
+
+let gastly;
+
+//API Call for Gastly
+
+axios.get("https://pokeapi.co/api/v2/pokemon/92/").then((response) => {
+    let data = response.data
+    let pokemonName = data.name
+    let abilities = data.abilities.map( (element) => element.ability.name)
+    let moves = data.moves.map((element) =>  element.move.name)
+    let types = data.types.map((element) => element.type.name)
+  
+     gastly = new Pokemon(pokemonName, abilities,moves,types)
+})
+
+
+
+
+
+
+let haunter;
+
+// API call for Haunter
+
+axios.get("https://pokeapi.co/api/v2/pokemon/93/").then((response) => {
+    let data = response.data
+    let pokemonName = data.name
+    let abilities = data.abilities.map( (element) => element.ability.name)
+    let moves = data.moves.map((element) =>  element.move.name)
+    let types = data.types.map((element) => element.type.name)
+
+
+     haunter = new Pokemon(pokemonName, abilities,moves,types)
 })
