bea6138f51bff338136e0a7e41d1a1fbc3efd63f
imani alexis <alexis.bailey37@gmail.com>
2018-07-16 09:35:27 -0400

    updated stats to include whats in the reqs

--- main.js
+++ main.js
@@ -9,11 +9,16 @@
 
 
 class Pokemon {
-    constructor(name,abilities,moves,types) {
-        this.name = name
-        this.abilities = abilities
-        this.moves = moves
-        this.types = types
+    constructor(name,hp,attack,defense,abilities) {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.abilities = abilities;
+
+
+        // this.moves = moves
+        // this.types = types
     }
 }
 
@@ -25,13 +30,20 @@ let golbat;
 
 // Golbat API Call
 axios.get("https://pokeapi.co/api/v2/pokemon/42/").then((response) => {
-    let data = response.data
-    let pokemonName = data.name
-    let abilities = data.abilities.map( (element) => element.ability.name)
-    let moves = data.moves.map((element) =>  element.move.name)
-    let types = data.types.map((element) => element.type.name)
-  
-     golbat = new Pokemon(pokemonName, abilities,moves,types)
+    let data = response.data;
+    let pokemonName = data.name;
+    let hp = data.stats[5].base_stat;
+    let attack = data.stats[4].base_stat;
+    let defense = data.stats[3].base_stat;
+    let abilities = data.abilities.map( (element) => element.ability.name);
+
+
+    // let moves = data.moves.map((element) =>  element.move.name)
+    // let types = data.types.map((element) => element.type.name)
+   
+    console.log(data)
+
+     golbat = new Pokemon(name,hp,attack,defense,abilities)
 })
 
 
@@ -41,13 +53,14 @@ let gastly;
 //API Call for Gastly
 
 axios.get("https://pokeapi.co/api/v2/pokemon/92/").then((response) => {
-    let data = response.data
-    let pokemonName = data.name
+    let data = response.data;
+    let pokemonName = data.name;
+    let hp = data.stats[5].base_stat;
+    let attack = data.stats[4].base_stat;
+    let defense = data.stats[3].base_stat;
     let abilities = data.abilities.map( (element) => element.ability.name)
-    let moves = data.moves.map((element) =>  element.move.name)
-    let types = data.types.map((element) => element.type.name)
   
-     gastly = new Pokemon(pokemonName, abilities,moves,types)
+     gastly = new Pokemon(name,hp,attack,defense,abilities)
 })
 
 
@@ -60,12 +73,13 @@ let haunter;
 // API call for Haunter
 
 axios.get("https://pokeapi.co/api/v2/pokemon/93/").then((response) => {
-    let data = response.data
-    let pokemonName = data.name
-    let abilities = data.abilities.map( (element) => element.ability.name)
-    let moves = data.moves.map((element) =>  element.move.name)
-    let types = data.types.map((element) => element.type.name)
+    let data = response.data;
+    let pokemonName = data.name;
+    let hp = data.stats[5].base_stat;
+    let attack = data.stats[4].base_stat;
+    let defense = data.stats[3].base_stat;
+    let abilities = data.abilities.map( (element) => element.ability.name);
 
 
-     haunter = new Pokemon(pokemonName, abilities,moves,types)
+     haunter = new Pokemon(name,hp,attack,defense,abilities)
 })
