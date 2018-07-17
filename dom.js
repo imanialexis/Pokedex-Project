39dd2aa8ed2143bfd3fafa98f149075b6953b0a7
imani alexis <alexis.bailey37@gmail.com>
2018-07-17 16:04:12 -0400

    added right button event

--- dom.js
+++ dom.js
@@ -11,3 +11,15 @@ let abilities = document.getElementById('abilities');
 
 
 
+rightButton.addEventListener('click', (event) => {
+    for(let i = 0; i < pokemonCounter.length; i++) {
+        name.innerText = pokemonCounter[i].name;
+        hp.innerText = pokemonCounter[i].hp;
+        attack.innerText = pokemonCounter[i].attack;
+        defense.innerText = pokemonCounter[i].defense;
+        abilities.innerText = pokemonCounter[i].abilities[i]
+    }
+
+})
+
+
