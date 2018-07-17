2f66c6e75ed50334a1fbdcd2d0466638735b2999
imani alexis <alexis.bailey37@gmail.com>
2018-07-17 09:26:38 -0400

    formatting before push to master

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
 
 
 class Pokemon {
-    constructor(name,hp,attack,defense,abilities,image) {
+    constructor(name, hp, attack, defense, abilities, image) {
         this.name = name;
         this.hp = hp;
         this.attack = attack;
@@ -36,17 +36,12 @@ axios.get("https://pokeapi.co/api/v2/pokemon/42/").then((response) => {
     let hp = data.stats[5].base_stat;
     let attack = data.stats[4].base_stat;
     let defense = data.stats[3].base_stat;
-    let abilities = data.abilities.map( (element) => element.ability.name);
+    let abilities = data.abilities.map((element) => element.ability.name);
     let image = data.sprites.front_default;
-    
-
-
     // let moves = data.moves.map((element) =>  element.move.name)
     // let types = data.types.map((element) => element.type.name)
-   
-    console.log(data)
 
-     golbat = new Pokemon(name,hp,attack,defense,abilities,image)
+    golbat = new Pokemon(name, hp, attack, defense, abilities, image)
 })
 
 
@@ -61,10 +56,10 @@ axios.get("https://pokeapi.co/api/v2/pokemon/92/").then((response) => {
     let hp = data.stats[5].base_stat;
     let attack = data.stats[4].base_stat;
     let defense = data.stats[3].base_stat;
-    let abilities = data.abilities.map( (element) => element.ability.name);
+    let abilities = data.abilities.map((element) => element.ability.name);
     let image = data.sprites.front_default;
-  
-     gastly = new Pokemon(name,hp,attack,defense,abilities,image)
+
+    gastly = new Pokemon(name, hp, attack, defense, abilities, image)
 })
 
 
@@ -82,10 +77,8 @@ axios.get("https://pokeapi.co/api/v2/pokemon/93/").then((response) => {
     let hp = data.stats[5].base_stat;
     let attack = data.stats[4].base_stat;
     let defense = data.stats[3].base_stat;
-    let abilities = data.abilities.map( (element) => element.ability.name);
+    let abilities = data.abilities.map((element) => element.ability.name);
     let image = data.sprites.front_default;
 
-
-
-     haunter = new Pokemon(name,hp,attack,defense,abilities,image)
+    haunter = new Pokemon(name, hp, attack, defense, abilities, image)
 })
